17cde0988ec6ba45bbf2e79b6444d7c9bdc42780
McKathlin <mckathlin@gmail.com>
2024-10-28 15:54:37 +0000

    Require sumAll arguments to be whole numbers >= 0

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,20 @@
 const sumAll = function(startNum, endNum) {
-    if (typeof startNum != "number" || typeof endNum != "number") {
+    // Require both parameters to be non-negative integers
+    if (!isWholeNumber(startNum) || !isWholeNumber(endNum)) {
         return "ERROR";
     }
+
+    // Find the sum
     let sum = 0;
     for (let i = startNum; i <= endNum; i++) {
         sum += i;
     }
     return sum;
 };
+
+const isWholeNumber = function(n) {
+    return typeof(n) == "number" && Number.isInteger(n) && n >= 0;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
