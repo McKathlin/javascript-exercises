e23f16f83a43bc1f452367e3b9465f2cb1d87ccd
McKathlin <mckathlin@gmail.com>
2024-10-28 17:04:20 +0000

    Round tempConversion results to 1 decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
 const convertToCelsius = function(fahrenheit) {
-  return (fahrenheit - 32) * (5/9);
+  return roundToDecimal((fahrenheit - 32) * (5/9), 1);
 };
 
 const convertToFahrenheit = function(celsius) {
-  return (9/5) * celsius + 32;
+  return roundToDecimal((9/5) * celsius + 32, 1);
+};
+
+const roundToDecimal = function(num, decimalPlaces) {
+  const multiplier = 10**decimalPlaces;
+  return Math.round(num * multiplier) / multiplier;
 };
 
 // Do not edit below this line
