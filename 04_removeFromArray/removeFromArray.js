da9900416785b3cff2fc8d62ce4963c1f1f0b744
McKathlin <mckathlin@gmail.com>
2024-10-28 15:35:17 +0000

    Implement removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...valuesToRemove) {
+    const removalSet = new Set(valuesToRemove);
+    const arrCopy = [];
+    for (element of arr) {
+        if (!removalSet.has(element)) {
+            arrCopy.push(element);
+        }
+    }
+    return arrCopy;
 };
 
 // Do not edit below this line
