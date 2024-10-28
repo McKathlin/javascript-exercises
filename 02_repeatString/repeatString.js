25221765927cbdc0cd3998452331bb1c9ddb952a
McKathlin <mckathlin@gmail.com>
2024-10-28 15:15:37 +0000

    Implement repeatString and un-skip all its tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    if (count < 0) {
+        return "ERROR";
+    }
+    let repeats = new Array(count);
+    for (let i = 0; i < count; i++) {
+        repeats[i] = str;
+    }
+    return repeats.join('');
 };
 
 // Do not edit below this line
