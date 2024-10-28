8793d2b7296995808ad6e5f1ace0fcabf4d115da
McKathlin <mckathlin@gmail.com>
2024-10-28 15:21:46 +0000

    Implement reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(original) {
+    let chars = new Array(original.length);
+    for (let i = 0; i < original.length; i++) {
+        let reverseIndex = original.length - 1 - i;
+        chars[reverseIndex] = original[i];
+    }
+    return chars.join('');
 };
 
 // Do not edit below this line
