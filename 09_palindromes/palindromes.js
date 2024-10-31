15a1e25b1783b48c60efc6ae296c2a4d82ca9a80
McKathlin <mckathlin@gmail.com>
2024-10-31 15:15:06 +0000

    Write palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function(text) {
+    // Remove all non-alphanumeric characters.
+    const checkableText = text.split(/\W+/).join('').toLowerCase();
 
+    // Check for symmetry.
+    const midpoint = Math.floor(checkableText.length / 2);
+    const last = checkableText.length - 1;
+    for (let i = 0; i < midpoint; i++) {
+        if (checkableText[i] != checkableText[last - i]) {
+            return false; // Mismatch found.
+        }
+    }
+    // Everything was checked and looks good!
+    return true;
 };
 
 // Do not edit below this line
