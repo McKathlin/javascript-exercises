2f7416673afc8a1d614d40a7d93ea1c8ad442526
McKathlin <mckathlin@gmail.com>
2024-10-31 16:02:48 +0000

    Implement findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const findAge = function(person) {
+        const endYear = person.yearOfDeath ?? new Date().getFullYear();
+        return endYear - person.yearOfBirth;
+    }
+    return people.reduce(
+        (oldest, me) => findAge(me) > findAge(oldest) ? me : oldest,
+        people[0]
+    );
 };
 
 // Do not edit below this line
