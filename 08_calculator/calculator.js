df93ba89e15cdeee6696de6c82dc4d2e1415ad8b
McKathlin <mckathlin@gmail.com>
2024-10-31 14:22:44 +0000

    Draft all calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	return numArray.reduce((theSum, num) => (theSum + num), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return numArray.reduce((product, num) => (num * product), 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let product = 1;
+	for (let i = 2; i <= num; i++) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
