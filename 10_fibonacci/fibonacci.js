06beb33074c797f1e11e47c5108513e5dade8778
McKathlin <mckathlin@gmail.com>
2024-10-31 15:39:13 +0000

    Code fibonacci function
    This function's algorithm is iterative and does not store a lookup.
    It runs in O(n) time an O(1) space, where n is the number passed in.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let latest = 1;
+    let older = 0;
+    let current = 1;
+    for (let i = 1; i < num; i++) {
+        current = latest + older;
+        older = latest;
+        latest = current;
+    }
+    return current;
 };
 
 // Do not edit below this line
